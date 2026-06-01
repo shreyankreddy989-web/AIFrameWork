06952a00b7765d8c8b41100af118174ca6ada7e7
ADMIN <admin@ADMINs-MBP.lan>
2026-06-01 23:13:59 +0530

    second commit

---
+++ src/test/java/com/tests/LoginpageTests.java
@@ -0,0 +1,34 @@
+package com.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginpageTests {
+
+
+       public static void main(String[] args) throws InterruptedException {
+
+
+           WebDriverManager.chromedriver().setup();
+           WebDriver driver = new ChromeDriver();
+           driver.manage().window().maximize();
+
+          driver.get("https://google.com");
+           driver.findElement(By.name("q")).sendKeys("Automation");
+
+           Thread.sleep(6000);
+
+
+        driver.get("https://google.com");
+        driver.findElement(By.name("q")).sendKeys("testing mini bytes");
+
+        Thread.sleep(6000);
+           driver.quit();
+
+    }
+
+
+}
