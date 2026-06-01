8b50208dd5de8c15b72d58e5b969f93a78e346ef
ADMIN <admin@ADMINs-MBP.lan>
2026-06-01 23:21:12 +0530

    changed thread time

--- src/test/java/com/tests/LoginpageTests.java
+++ src/test/java/com/tests/LoginpageTests.java
@@ -25,7 +25,7 @@ public class LoginpageTests {
         driver.get("https://google.com");
         driver.findElement(By.name("q")).sendKeys("testing mini bytes");
 
-        Thread.sleep(5000);
+        Thread.sleep(3500);
            driver.quit();
 
 
